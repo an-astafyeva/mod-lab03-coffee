dcb15d256406b7c2c54c844aafd8df7aaffb2021
an-astafyeva <astafeva22082004@gmail.com>
2026-06-28 15:08:55 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,131 @@
-// Copyright 2022 GHA Test Team
+// Copyright 2026 Astafeva Anastasiia
 
-#include <gtest/gtest.h>
 #include "Automata.h"
+#include <gtest/gtest.h>
+#include <string>
+#include <vector>
+
+TEST(correct_transition, test1_initial_off) {
+    Automata a;
+    EXPECT_EQ(OFF, a.getState());
+}
+
+TEST(correct_transition, test2_on) {
+    Automata a;
+    a.on();
+    EXPECT_EQ(WAIT, a.getState());
+}
+
+TEST(correct_transition, test3_off) {
+    Automata a;
+    a.on();
+    a.off();
+    EXPECT_EQ(OFF, a.getState());
+}
+
+TEST(correct_transition, test4_coin_from_wait) {
+    Automata a;
+    a.on();
+    a.coin();
+    EXPECT_EQ(ACCEPT, a.getState());
+}
+
+TEST(correct_transition, test5_coin_from_accept) {
+    Automata a;
+    a.on();
+    a.coin();
+    a.coin();
+    EXPECT_EQ(ACCEPT, a.getState());
+}
+
+TEST(correct_transition, test6_coin_from_off) {
+    Automata a;
+    a.coin();
+    EXPECT_EQ(OFF, a.getState());
+}
+
+TEST(correct_transition, test7_choice) {
+    Automata a;
+    a.on();
+    a.coin();
+    a.choice("Tea");
+    EXPECT_EQ(CHECK, a.getState());
+}
+
+TEST(correct_transition, test8_check_enough) {
+    Automata a;
+    a.on();
+    a.coin();
+    a.coin();
+    a.choice("Tea");
+    a.check();
+    EXPECT_EQ(COOK, a.getState());
+}
+
+TEST(correct_transition, test9_check_not_enough) {
+    Automata a;
+    a.on();
+    a.coin();
+    a.choice("Tea");
+    a.check();
+    EXPECT_EQ(ACCEPT, a.getState());
+}
+
+TEST(correct_transition, test10_cook) {
+    Automata a;
+    a.on();
+    a.coin();
+    a.coin();
+    a.choice("Tea");
+    a.check();
+    a.cook();
+    EXPECT_EQ(COLLECT, a.getState());
+}
+
+TEST(correct_transition, test11_finish) {
+    Automata a;
+    a.on();
+    a.coin();
+    a.coin();
+    a.choice("Tea");
+    a.check();
+    a.cook();
+    a.finish();
+    EXPECT_EQ(WAIT, a.getState());
+}
+
+TEST(correct_transition, test12_cancel_from_accept) {
+    Automata a;
+    a.on();
+    a.coin();
+    a.cancel();
+    EXPECT_EQ(WAIT, a.getState());
+}
+
+TEST(correct_transition, test13_cancel_from_check) {
+    Automata a;
+    a.on();
+    a.coin();
+    a.choice("Tea");
+    a.cancel();
+    EXPECT_EQ(WAIT, a.getState());
+}
+
+TEST(correct_transition, test14_cash_deducted) {
+    Automata a;
+    a.on();
+    a.coin();
+    a.coin();
+    a.coin();
+    a.choice("Coffee");
+    a.check();
+    EXPECT_EQ(0, a.getCash());
+}
+
+TEST(correct_transition, test15_unknown_item) {
+    Automata a;
+    a.on();
+    a.coin();
+    a.choice("Lemonade");
+    EXPECT_EQ(ACCEPT, a.getState());
+}
